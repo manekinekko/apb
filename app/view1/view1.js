6bd6e8e7ee76715ac1b3d8f67fdc9c45a2486604
Wassim Chegham <github@wassimchegham.com>
2016-09-02 00:20:10 +0200

    imporve use cases

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('app.view1', ['ngRoute'])
+angular.module('app.view1', ['ngRoute', 'robdodson.ce-bind'])
 
 .config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl as ctrl1'
+    controller: 'View1Ctrl as $ctrl'
   });
 
 }])
@@ -17,8 +17,8 @@ angular.module('app.view1', ['ngRoute'])
 
   this.model = {};
 
-  this.OnSelectedItemChanged = (selectedIndex) => {
-    this.selectedIndex = selectedIndex + 1;
+  this.OnSelectedItemChanged = (selectedEvent) => {
+    this.selectedIndex = selectedEvent.detail + 1;
   }
 
   this.btn1 = (event) => {
@@ -42,12 +42,12 @@ angular.module('app.view1', ['ngRoute'])
 .directive('bindPolymerEvent', ['$rootScope', function($rootScope) {
   return {
     restrict: 'A',
-    link: function(s,e,a,c) {
-      e.on('x-on-selected-item-changed', (e) => {
+    link: function(s, e, a, c) {
+      e.on('selected-item-changed', (e) => {
 
         // event coming from Polymer
-        $rootScope.$apply( () => {
-          s.ctrl1.OnSelectedItemChanged(e.detail.value);
+        $rootScope.$apply(() => {
+          s.$ctrl.OnSelectedItemChanged(e);
         });
       })
     }
